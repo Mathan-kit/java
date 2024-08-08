580765dc6ebdd23a7663bba4c0abb872acc12090
Mathan Kumar M <82459169+Mathan-kit@users.noreply.github.com>
2024-08-08 22:16:45 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,18 @@
+public class array 
+{
+    public static void main(String[] args) 
+    {
+        int a[] = {11,12,13,14,15,16,17,18,19,20};
+        System.out.println(a[9]);
+        System.out.println(a[8]);
+        System.out.println(a[7]);
+        System.out.println(a[6]);
+        System.out.println(a[5]);
+        System.out.println(a[4]);
+        System.out.println(a[3]);
+        System.out.println(a[2]);
+        System.out.println(a[1]);
+        System.out.println(a[0]);
+    }
+    
+}
